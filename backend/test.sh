3afc812d72c0d5c1b54941c23c4cdf9f6a6626e2
pnagy <pn@blackwoodseven.com>
2018-04-01 22:20:07 +0200

    remove accident file

--- backend/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for number in {1..12}
-do
-`cp ./static/music/Street_Dogs-Back_to_the_World-2005/furelise.mp3 ./static/music/Street_Dogs-Back_to_the_World-2005/$number.mp3`
-done
-exit 0
